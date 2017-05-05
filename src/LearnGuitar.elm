c5af7a138ce158ecf9f9b6c368e816a74964a661
Unknown <christopher.nyberg@gmail.com>
2017-05-05 00:05:20 -0700

    Getting very rough start

--- src/LearnGuitar.elm
+++ src/LearnGuitar.elm
@@ -1,9 +1,11 @@
 module LearnGuitar exposing (..)
 
-import Html exposing (Html)
-import Svg exposing (..)
+import Html exposing (..)
+import Svg exposing (circle, svg, line)
 import Svg.Attributes exposing (..)
-import Time exposing (Time, second)
+
+
+--import MusicTheory.MusicTypes exposing (..)
 
 
 main : Program Never Model Msg
@@ -17,16 +19,72 @@ main =
 
 
 
+-- TYPES
+
+
+type Pitch
+    = A
+    | B
+    | C
+    | D
+    | E
+    | F
+    | G
+
+
+type Note
+    = Note Pitch
+    | Accidental Pitch Pitch
+
+
+type Scale
+    = Major
+    | MelodicMinor
+    | HarmonicMinor
+
+
+chromaticScale : List Note
+chromaticScale =
+    [ Note A
+    , Accidental A B
+    , Note B
+    , Note C
+    , Accidental C D
+    , Note D
+    , Accidental D E
+    , Note E
+    , Note F
+    , Accidental F G
+    , Note G
+    , Accidental G A
+    ]
+
+
+listOfScales : List Scale
+listOfScales =
+    [ Major
+    , MelodicMinor
+    , HarmonicMinor
+    ]
+
+
+
 -- MODEL
 
 
 type alias Model =
-    Time
+    { currentScale : Note
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( initModel, Cmd.none )
+
+
+initModel : Model
+initModel =
+    { currentScale = Note C }
 
 
 
@@ -34,14 +92,14 @@ init =
 
 
 type Msg
-    = Tick Time
+    = NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Tick newTime ->
-            ( newTime, Cmd.none )
+        NoOp ->
+            ( model, Cmd.none )
 
 
 
@@ -50,7 +108,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    Sub.none
 
 
 
@@ -59,17 +117,64 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    let
-        angle =
-            turns (Time.inMinutes model)
+    div
+        []
+        [ rootNoteDropdown model
+        , scaleDropdown model
+        , br [] []
+        , div [] [ text "The scale is: " ]
+        , displayScale model
+        ]
 
-        handX =
-            toString (50 + 40 * cos angle)
 
-        handY =
-            toString (50 + 40 * sin angle)
-    in
-        svg [ viewBox "0 0 100 100", width "300px" ]
-            [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-            , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
-            ]
+rootNoteDropdown : Model -> Html Msg
+rootNoteDropdown model =
+    dropDownMaker "Please select a root note" chromaticScale noteMaker
+
+
+scaleDropdown : Model -> Html Msg
+scaleDropdown model =
+    dropDownMaker "Please select a root scale type" listOfScales scaleOptionMaker
+
+
+dropDownMaker : String -> List a -> (a -> Html Msg) -> Html Msg
+dropDownMaker labelText stuff stuffMaker =
+    div
+        []
+        [ label
+            []
+            [ text labelText ]
+        , select
+            []
+            (List.map stuffMaker stuff)
+        ]
+
+
+noteMaker : Note -> Html Msg
+noteMaker note =
+    option
+        []
+        [ text <| toString note ]
+
+
+scaleOptionMaker : Scale -> Html Msg
+scaleOptionMaker scale =
+    option
+        []
+        [ text <| toString scale ]
+
+
+displayScale : Model -> Html Msg
+displayScale model =
+    div
+        []
+        [ text <| toString chromaticScale ]
+
+
+svgThingy : Model -> Html Msg
+svgThingy model =
+    svg
+        [ viewBox "0 0 100 100", width "300px" ]
+        [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+        , line [ x1 "50", y1 "50", x2 "10", y2 "15", stroke "#023963" ] []
+        ]
